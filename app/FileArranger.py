971c33a8291941cb1400bc7e5e5d8ba49f1dc374
kuba <krob2610@gmail.com>
2023-12-19 23:31:23 +0100

    inital work on FileArranger

---
+++ app/FileArranger.py
@@ -0,0 +1,71 @@
+import logging
+import os
+import zipfile
+from pathlib import Path
+
+import pandas as pd
+
+from .device_namespace import DEVICE_MAP, INPUT_PATH, NEW_INPUT, SENSORS
+
+logging.basicConfig(level=logging.INFO)
+
+
+class FileArranger:
+    def __init__(self):
+        self._save_data_to_coresponding_file()
+
+    def _save_data_to_coresponding_file(self) -> None:
+        names = self._get_folder_names()
+
+        for name in names:
+            self._extract_zip(name)
+
+    def _get_folder_names(self) -> list[str]:
+        """Get names of folders in input_data/!new_input
+
+        Returns
+        -------
+        list[str]
+            list with names of folder for each device
+        """
+
+        path = Path(INPUT_PATH, NEW_INPUT)
+        return [file.name for file in path.iterdir() if file.suffix == ".zip"]
+
+    def _extract_zip(self, path: str) -> None:
+        """Extract zip file to specific folder
+
+        Parameters
+        ----------
+        path : str
+            path to zip file
+        """
+
+        base_name = os.path.basename(Path(INPUT_PATH) / Path(NEW_INPUT) / path)
+        date = base_name.split("_")[0]
+
+        device_name = DEVICE_MAP[base_name.split("-")[-1][:-4]]
+
+        # Create a new folder with the same name as the zip file
+        new_date_directory = Path(INPUT_PATH) / date / device_name
+        new_date_directory.mkdir(parents=True, exist_ok=True)
+        logging.info(f"Device: {device_name} with date: {date} will be saved to folder")
+
+        with zipfile.ZipFile(Path(INPUT_PATH) / Path(NEW_INPUT) / path, "r") as zip_ref:
+            # Extract all the files into the new folder
+            # Log the names of all the files in the zip file
+            for filename in zip_ref.namelist():
+                if filename[:-4] not in SENSORS:
+                    logging.warning(f"No sensor data for : {filename[:-4]}")
+
+            zip_ref.extractall(new_date_directory)
+
+        file_path = new_date_directory / filename
+        if os.stat(file_path).st_size == 0:
+            logging.warning(f"The file {filename} is empty")
+
+    def delete_new_inputs(self) -> None:
+        """delate input files from !new_input folder"""
+        path = Path(INPUT_PATH, NEW_INPUT)
+        for file in path.iterdir():
+            os.remove(file)
---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+# Inside app/__init__.py
+from .device_namespace import *
+from .FileArranger import FileArranger
