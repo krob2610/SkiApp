41741d622fd5225cac7be00d292d572979f877f3
kuba <krob2610@gmail.com>
2023-12-24 14:40:37 +0100

    new display for missing devices

--- app/FileArranger.py
+++ app/FileArranger.py
@@ -20,7 +20,10 @@ logging.basicConfig(level=logging.INFO)
 
 class FileArranger:
     def __init__(self):
+        self.dates = []
+        self.dates_dirs = []
         self._save_data_to_coresponding_file()
+        self.check_files_saved()
 
     def _save_data_to_coresponding_file(self) -> None:
         names = self._get_folder_names()
@@ -51,31 +54,54 @@ class FileArranger:
 
         base_name = os.path.basename(Path(INPUT_PATH) / Path(NEW_INPUT) / path)
         date = base_name.split("_")[0]
+        self._append_date(date)
+        self._append_date_dir(Path(INPUT_PATH) / date)
 
         device_name = DEVICE_MAP[base_name.split("-")[-1][:-4]]
 
-        # Create a new folder with the same name as the zip file
         new_date_directory = Path(INPUT_PATH) / date / device_name
+
         new_date_directory.mkdir(parents=True, exist_ok=True)
-        logging.info(f"Device: {device_name} with date: {date} will be saved to folder")
 
         with zipfile.ZipFile(Path(INPUT_PATH) / Path(NEW_INPUT) / path, "r") as zip_ref:
             # Extract all the files into the new folder
             # Log the names of all the files in the zip file
-            for filename in zip_ref.namelist():
-                if filename[:-4] not in SENSORS:
-                    logging.warning(f"No sensor data for : {filename[:-4]}")
+            # for filename in zip_ref.namelist():
+            #     if filename[:-4] not in SENSORS:
+            #         logging.warning(f"No sensor data for : {filename[:-4]}")
 
             zip_ref.extractall(new_date_directory)
 
-        file_path = new_date_directory / filename
-        if os.stat(file_path).st_size == 0:
-            logging.warning(f"The file {filename} is empty")
+            # file_path = new_date_directory / filename
+            # if os.stat(file_path).st_size == 0:
+            #     logging.warning(f"The file {filename} is empty")
 
         # add offset to txt file
-        self.add_offset(device_name, new_date_directory)
+        self._add_offset(device_name, new_date_directory)
 
-    def add_offset(self, device: str, path: Path) -> None:
+    def _append_date(self, date: str) -> None:
+        """append date to self.dates if not already there
+
+        Parameters
+        ----------
+        date : str
+            date to be appended
+        """
+        if date not in self.dates:
+            self.dates.append(date)
+
+    def _append_date_dir(self, date_dir: str) -> None:
+        """append date_dir to self.dates_dirs if not already there
+
+        Parameters
+        ----------
+        date_dir : str
+            date_dir to be appended
+        """
+        if date_dir not in self.dates_dirs:
+            self.dates_dirs.append(date_dir)
+
+    def _add_offset(self, device: str, path: Path) -> None:
         """Add offset to specific device into txt file in miliseconds
 
         Parameters
@@ -86,8 +112,29 @@ class FileArranger:
             path where the offset.txt file should be created
         """
         DEVICE_NUMBER[device]
-        with open(path / "offset.txt", "a") as f:
+        with open(path / "offset.txt", "w") as f:
             f.write(f"{DEVICE_TIME_OFFSET[DEVICE_NUMBER[device]]}\n")
+
+    def check_files_saved(self) -> None:
+        """Check if all files were saved to specific folder"""
+        total_devices = len(DEVICE_MAP.values())
+        for date_dir in self.dates_dirs:
+            non_missing_devices = 0
+            missing_devices = []
+            for device in DEVICE_MAP.values():
+                if not os.path.exists(date_dir / device):
+                    missing_devices.append(device)
+                else:
+                    non_missing_devices += 1
+            print(f"\nChecking {date_dir}:")
+            if non_missing_devices == total_devices:
+                print(f"Devices: {non_missing_devices}/{total_devices} ✅")
+            else:
+                print(f"Devices: {non_missing_devices}/{total_devices} 🔥")
+            if missing_devices:
+                print("Missing devices:")
+                for device in missing_devices:
+                    print(f"{device}: ❌")
 
     def delete_new_inputs(self) -> None:
         """delate input files from !new_input folder"""
